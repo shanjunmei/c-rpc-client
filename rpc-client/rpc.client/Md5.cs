b2ac67359ab0fa98a6dfc22093bb4b94c0cf4300
hz15101769 <445052471@qq.com>
2016-01-27 14:00:43 +0800

    增加配置类，md5加密工具类，测试类

---
+++ rpc-client/rpc.client/Md5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpc_client.rpc.client
+{
+    class Md5
+    {
+
+        public static string encrypt(string src)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(System.Text.Encoding.Default.GetBytes(src));
+            string x = BitConverter.ToString(result).Replace("-", "").ToLower();
+            return x;
+
+
+        }
+
+
+    }
+}
